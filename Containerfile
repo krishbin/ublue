1c15bc278a681ca85989125d631f78c33b48d3b2
Krishbin Paudel <krishbinp@outlook.com>
2024-05-31 08:55:24 +0545

    added signed container capabilities

--- Containerfile
+++ Containerfile
@@ -54,6 +54,7 @@ RUN find /tmp/akmods-rpms
 ### 3. MODIFICATIONS
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
+COPY cosign.pub /usr/share/ublue-os/cosign.pub
 
 COPY build.sh /tmp/build.sh
 ADD --chmod=0755 scripts/* /tmp/
--- build.sh
+++ build.sh
@@ -37,11 +37,32 @@ rpm-ostree install git-lfs git-crypt libgit2
 # flatpak uninstall -y --system org.mozilla.Firefox
 
 # flatpak uninstall --unused -y
+rpm-ostree install neovim
 
 /tmp/install-1password.sh
 /tmp/install-firefox.sh
 /tmp/install-chrome.sh
 
-rpm-ostree install gh neovim
+# add cosign pub key
+cp /usr/share/ublue-os/cosign.pub /usr/etc/pki/containers/cosign-krish.pub
+FILE=/usr/etc/containers/policy.json
+TMP=/tmp/policy.json
 
-rm -rf /usr/share/applications/nvim.desktop /usr/share/applications/remote-viewer.desktop
+jq '.transports.docker |= 
+    {"ghcr.io/krishbin": [
+            {
+                "type": "sigstoreSigned",
+                "keyPath": "/usr/etc/pki/containers/cosign-krish.pub",
+                "signedIdentity": {
+                    "type": "matchRepository"
+                }
+            }
+        ]
+    }
++ .' $FILE > $TMP
+mv -f $TMP $FILE
+
+cp /usr/etc/containers/registries.d/ublue-os.yaml /usr/etc/containers/registries.d/ublue-krish.yaml
+sed -i "s ghcr.io/ublue-os ghcr.io/krishbin g" /usr/etc/containers/registries.d/ublue-krish.yaml
+
+rm -rf /usr/share/applications/nvim.desktop /usr/share/applications/remote-viewer.desktop /usr/share/applications/input-remapper-gtk.desktop
