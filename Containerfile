a0c51a97cbf2b03f8826da0858d771abd38f0ee2
Krishbin Paudel <krishbinp@outlook.com>
2024-05-31 11:43:19 +0545

    disabled kmod

--- Containerfile
+++ Containerfile
@@ -45,11 +45,11 @@ ARG SOURCE_TAG="latest"
 FROM ghcr.io/ublue-os/${SOURCE_IMAGE}${SOURCE_SUFFIX}:${SOURCE_TAG}
 
 
-COPY --from=ghcr.io/ublue-os/akmods:main-40 /rpms/ /tmp/akmods-rpms/
+# COPY --from=ghcr.io/ublue-os/akmods:main-40 /rpms/ /tmp/akmods-rpms/
 
-RUN find /tmp/akmods-rpms
-RUN rpm-ostree install /tmp/akmods-rpms/ublue-os/ublue-os-akmods*.rpm
-RUN rpm-ostree install /tmp/akmods-rpms/kmods/kmod-v4l2loopback*.rpm
+# RUN find /tmp/akmods-rpms
+# RUN rpm-ostree install /tmp/akmods-rpms/ublue-os/ublue-os-akmods*.rpm
+# RUN rpm-ostree install /tmp/akmods-rpms/kmods/kmod-v4l2loopback*.rpm
 
 ### 3. MODIFICATIONS
 ## make modifications desired in your image and install packages by modifying the build.sh script
--- build.sh
+++ build.sh
@@ -18,25 +18,7 @@ RELEASE="$(rpm -E %fedora)"
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 rpm-ostree install git-lfs git-crypt libgit2
-# rpm-ostree install kmod-openrazer kmod-xpadneo kmod-v4l2loopback kmod-xone 
 
-# for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
-#   echo "akmods: enable default entry: ${REPO}"
-#   sed -i.bak '0,/enabled=0/{s/enabled=0/enabled=1/}' ${REPO}
-# done
-# rpm-ostree install /tmp/akmods-rpms/*.rpm
-# for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
-#   echo "akmods: restore defaults: ${REPO}"
-#   mv ${REPO}.bak ${REPO}
-# done
-
-# flatpak uninstall -y --system org.fedoraproject.MediaWriter
-# flatpak uninstall -y --system org.gnome.Calendar
-# flatpak uninstall -y --system org.gnome.Contacts
-# flatpak uninstall -y --system org.mozilla.Thunderbird
-# flatpak uninstall -y --system org.mozilla.Firefox
-
-# flatpak uninstall --unused -y
 rpm-ostree install neovim
 
 /tmp/install-1password.sh
