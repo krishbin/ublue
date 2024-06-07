c266991d520f78ab977721d7b90e2b7b58a566f7
Krishbin Paudel <krishbinp@outlook.com>
2024-06-07 16:46:29 +0545

    added vmware support

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ RELEASE="$(rpm -E %fedora)"
 
 rpm-ostree install git-lfs git-crypt libgit2
 
-rpm-ostree install neovim
+rpm-ostree install neovim kernel-devel
 
 /tmp/install-1password.sh
 /tmp/install-firefox.sh
@@ -45,3 +45,8 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/hardware:razer.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-obs-vkcapture.repo
 
 echo 'import "/usr/share/ublue-os/just/61-krish.just"' >> /usr/share/ublue-os/justfile
+
+echo 'Installing vmware modules'
+wget https://github.com/krishbin/vmware_modules/releases/download/17.5.2_6.8/VmWare-kmods-17.5.2-1.fc40.x86_64.rpm -O /tmp/VmWare-kmods-17.5.2-1.fc40.x86_64.rpm
+
+rpm-ostree install /tmp/VmWare-kmods-17.5.2-1.fc40.x86_64.rpm
