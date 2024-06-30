0f59543b6c88e63d2c3069d5c13e1592904175a2
Krishbin Paudel <krishbinp@outlook.com>
2024-06-30 15:02:46 +0545

    change script bugs

--- scripts/install-Jlink.sh
+++ scripts/install-Jlink.sh
@@ -6,9 +6,9 @@ JLINK_VERSION="796q"
 curl -X POST \
 -F "accept_license_agreement=accepted" \
 -F "submit=Download software" \
--o /tmp/JLink_Linux_V"${JLINK_VERSION}"_x86_64.tgz \
+-o JLink_Linux_V"${JLINK_VERSION}"_x86_64.tgz \
 https://www.segger.com/downloads/jlink/JLink_Linux_V"${JLINK_VERSION}"_x86_64.tgz
 
-tar xpvzf "/tmp/JLink_Linux_V${JLINK_VERSION}_x86_64.tgz"
+tar xpvzf "JLink_Linux_V${JLINK_VERSION}_x86_64.tgz"
 
-mv /tmp/JLink_Linux_V${JLINK_VERSION}_x86_64 /usr/lib/Jlink
+mv JLink_Linux_V${JLINK_VERSION}_x86_64 /usr/lib/JLink
