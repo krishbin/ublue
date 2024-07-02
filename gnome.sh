e4ac9325d67e2ce22367262b8e0bfcd323ad160a
Krishbin Paudel <krishbinp@outlook.com>
2024-07-02 12:13:39 +0545

    install kitty

--- gnome.sh
+++ gnome.sh
@@ -4,5 +4,5 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-rpm-ostree install i3 i3status dmenu i3lock xbacklight feh conky
+rpm-ostree install i3 i3status dmenu i3lock xbacklight feh conky kitty
 rpm-ostree install xss-lock picom network-manager-applet light maim xclip dunst polkit-gnome polybar rofi
