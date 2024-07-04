ffa15ed593c0f142cec8b588c9700632eb059d64
Krishbin Paudel <krishbinp@outlook.com>
2024-07-04 15:13:29 +0545

    added python3.11 for autorandr

--- gnome.sh
+++ gnome.sh
@@ -4,9 +4,9 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-rpm-ostree install i3 i3status dmenu i3lock xbacklight feh xev conky alacritty autorandr xautolock playerctl
+rpm-ostree install i3 i3status dmenu i3lock xbacklight feh xev conky alacritty autorandr xautolock playerctl python3.11
 rpm-ostree install xss-lock network-manager-applet light maim xclip dunst polkit-gnome polybar rofi
 
 # change /usr/bin/python3 to /usr/bin/python3.11 in /usr/bin/autorandr
-sed -i 's|/usr/bin/python3|/var/home/linuxbrew/.linuxbrew/bin/python3.11|g' /usr/bin/autorandr
+sed -i 's|/usr/bin/python3|/usr/bin/python3.11|g' /usr/bin/autorandr
 
